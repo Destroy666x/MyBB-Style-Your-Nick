d21512e2da401c2b612bbdad6b027efa6fb9fa3a
Przemek Pawlas <Destroy666@interia.pl>
2015-01-19 01:13:28 +0100

    Polish translation
    ...

---
+++ upload/inc/languages/polish/admin/style_your_nick_acp.lang.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+Nazwa: Stylizuj nazwę użytkownika
+Autor: Destroy666
+Wersja: 1.0
+Wymagania: Plugin Library, PostgreSQL 9.1
+Informacje: Plugin dla skryptu MyBB, zakodowany dla wersji 1.8.x (może także działać w 1.6.x/1.4.x).
+Pozwala zmieniać styl nazwy użytkownika w panelu (bazując na uprawnieniach grupy).
+1 edycja pliku źródłowego, 1 nowa tabela bazy danych, 14 nowych kolumn bazy danych, 8 nowych szablonów, 1 zmiana w szablonie, 10 nowych ustawień
+Licencja: GNU GPL v3, 29 June 2007. Więcej informacji w pliku LICENSE.md.
+Support: officjalne forum MyBB - http://community.mybb.com/mods.php?action=profile&uid=58253 (nie odpowiadam na PM, tylko na posty)
+Zgłaszanie błędów: mój github - https://github.com/Destroy666x
+
+© 2015 - date("Y")
+*/
+
+$l['style_your_nick'] = 'Stylizuj nazwę użytkownika';
+$l['style_your_nick_info'] = 'Pozwala zmieniać styl nazwy użytkownika w panelu (bazując na uprawnieniach grupy).';
+$l['pluginlibrary_missing'] = '<strong>Uwaga:</strong> Modyfikacja wymaga biblioteki Plugin Library do dodawania/usuwania szablonów. Można ją pobrać <a href="https://github.com/frostschutz/MyBB-PluginLibrary/archive/master.zip">tutaj</a>.';
+$l['core_changes_error'] = 'Wymagana zmiana w pliku źródłowym nie mogła zostać przeprowadzona.';
+
+$l['style_your_nick_settings'] = 'Ustawienia dla pluginu "Animowane dodatkowe grupy".';
+$l['style_your_nick_transparent'] = 'Zezwól na przezroczyste kolory?';
+$l['style_your_nick_transparent_desc'] = 'Ustaw na tak aby użytkownicy mogli korzystać z przezroczystych kolorów (transparent i RGBA).';
+$l['style_your_nick_max_font'] = 'Maksymalny rozmiar czcionki';
+$l['style_your_nick_max_font_desc'] = 'Podaj liczbę reprezentującą maksymalny rozmiar czcionki (w pikselach).';
+$l['style_your_nick_min_font'] = 'Minimalny rozmiar czcionki';
+$l['style_your_nick_min_font_desc'] = 'Podaj liczbę reprezentującą minimalny rozmiar czcionki (w pikselach).';
+$l['style_your_nick_max_shadowx'] = 'Maksymalna pozycja X cienia';
+$l['style_your_nick_max_shadowx_desc'] = 'Podaj liczbę reprezentującą maksymalną pozycję horyzontalna cienia (w pikselach).';
+$l['style_your_nick_min_shadowx'] = 'Minimalna pozycja X cienia';
+$l['style_your_nick_min_shadowx_desc'] = 'Podaj liczbę reprezentującą minimalną pozycję horyzontalną cienia (w pikselach).';
+$l['style_your_nick_max_shadowy'] = 'Maksymalna pozycja Y cienia';
+$l['style_your_nick_max_shadowy_desc'] = 'Podaj liczbę reprezentującą maksymalną pozycję wertykalną cienia (w pikselach).';
+$l['style_your_nick_min_shadowy'] = 'Minimalna pozycja Y cienia';
+$l['style_your_nick_min_shadowy_desc'] = 'Podaj liczbę reprezentującą minimalną pozycję wertykalną cienia (w pikselach).';
+$l['style_your_nick_max_shadowlength'] = 'Maksymalna długość cienia';
+$l['style_your_nick_max_shadowlength_desc'] = 'Podaj liczbę reprezentującą maksymalną długość cienia (w pikselach).';
+$l['style_your_nick_min_shadowlength'] = 'Minimalna długość cienia';
+$l['style_your_nick_min_shadowlength_desc'] = 'Podaj liczbę reprezentującą minimalną długość cienia (w pikselach).';
+$l['style_your_nick_max_backgroundimg'] = 'Maksymalne wymiary obrazka tła';
+$l['style_your_nick_max_backgroundimg_desc'] = 'Podaj maksymalną długość i wysokość obrazka tła, oddziel je znakiem "x" (w pikselach).';
+
+$l['style_your_nick_general'] = 'Generalne';
+$l['style_your_nick_can_style'] = 'Może stylizować nazwę użytkownika?<br />
+<small><strong>Uwaga:</strong> zaznaczenie tej opcji jest wymagane by reszta działała.</small>';
+$l['style_your_nick_use_default'] = "Użyj domyślnego stylu grupy jeśli użytkownik nie skorzysta ze własnej stylizacji?
+<small><strong>Uwaga:</strong> odznaczenie tej opcji nie jest zalecane dla dużych forów, gdyż zostanie użyte wolniejsze zapytanie do bazy.</small>";
+$l['style_your_nick_styling'] = 'Stylizacja';
+$l['style_your_nick_can_color'] = 'Może zmieniać kolor czcionki?';
+$l['style_your_nick_can_background'] = 'Może zmieniać kolor tła?';
+$l['style_your_nick_can_background_img'] = 'Może zmieniać obrazek tła?';
+$l['style_your_nick_can_background_repeat'] = 'Może włączyć powtarzanie się obrazka tła?';
+$l['style_your_nick_can_size'] = 'Może zmieniać rozmiar czcionki?';
+$l['style_your_nick_can_italic'] = 'Może zmieniać styl czcionki na kursywę?';
+$l['style_your_nick_can_bold'] = 'Może pogrubiać czionkę?';
+$l['style_your_nick_can_underline'] = 'Może podkreślać nazwę użytkownika?';
+$l['style_your_nick_can_overline'] = 'Może nadkreślać nazwę użytkownika?';
+$l['style_your_nick_can_strike'] = 'Może przekreślać nazwę użytkownika?';
+$l['style_your_nick_can_capital'] = 'Może zmieniać pierwszą literę każdego słowa nazwy użytkownika na dużą literę?';
+$l['style_your_nick_can_shadow'] = 'Może dodawać cień do nazwy użytkownika?';
---
+++ upload/inc/languages/polish/style_your_nick.lang.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+Nazwa: Stylizuj nazwę użytkownika
+Autor: Destroy666
+Wersja: 1.0
+Wymagania: Plugin Library, PostgreSQL 9.1
+Informacje: Plugin dla skryptu MyBB, zakodowany dla wersji 1.8.x (może także działać w 1.6.x/1.4.x).
+Pozwala zmieniać styl nazwy użytkownika w panelu (bazując na uprawnieniach grupy).
+1 edycja pliku źródłowego, 1 nowa tabela bazy danych, 14 nowych kolumn bazy danych, 8 nowych szablonów, 1 zmiana w szablonie, 10 nowych ustawień
+Licencja: GNU GPL v3, 29 June 2007. Więcej informacji w pliku LICENSE.md.
+Support: officjalne forum MyBB - http://community.mybb.com/mods.php?action=profile&uid=58253 (nie odpowiadam na PM, tylko na posty)
+Zgłaszanie błędów: mój github - https://github.com/Destroy666x
+
+© 2015 - date("Y")
+*/
+
+$l['style_your_nick'] = 'Stylizuj nazwę użytkownika';
+$l['style_your_nick_options'] = 'Opcje stylizacji';
+$l['style_your_nick_no_options'] = 'Aktualnie Twoja grupa nie ma dostępu do żadnej opcji stylizacji.';
+$l['style_your_nick_save'] = 'Zapisz';
+$l['style_your_nick_clear'] = 'Wyczyść';
+$l['style_your_nick_success'] = 'Stylizacja została z powodzeniem zmieniona. Zostaniesz teraz przekierowany na stronę edycji stylizacji.';
+$l['style_your_nick_clear_success'] = 'Stylizacja została z powodzeniem wyczyszczona. Zostaniesz teraz przekierowany na stronę edycji stylizacji.';
+
+$l['style_your_nick_desc_color'] = '<strong>Kolor czcionki</strong><br />
+Podaj prawidłowy kolor CSS. Wspierane formaty: ustalone nazwy (red, green, itd.), HEX #123DEF, RGB(255,255,255).';
+$l['style_your_nick_desc_background'] = '<strong>Kolor tła</strong><br />
+Podaj prawidłowy kolor CSS. Wspierane formaty: ustalone nazwy (red, green, itd.), HEX #123DEF, RGB(255,255,255).';
+$l['style_your_nick_desc_backgroundimg'] = '<strong>Obrazek w tle</strong><br />
+Podaj prawidłowy link do obrazka.';
+$l['style_your_nick_desc_backgroundrepeat'] = 'Tło ma się powtarzać w poziomie i pionie?';
+$l['style_your_nick_desc_size'] = '<strong>Rozmiar czcionki</strong><br />
+Podaj prawidłowy rozmiar. Wspierane formaty: 3px, 1.7cm, 14mm, 0.2in';
+$l['style_your_nick_desc_italic'] = 'Zmień styl czcionki na kursywę?';
+$l['style_your_nick_desc_bold'] = 'Pogrub czcionkę?';
+$l['style_your_nick_desc_underline'] = 'Podkreśl nazwę użytkownika?';
+$l['style_your_nick_desc_overline'] = 'Nadkreśl nazwę użytkownika?';
+$l['style_your_nick_desc_strike'] = 'Przekreśl nazwę użytkownika?';
+$l['style_your_nick_desc_capital'] = 'Pisz każde słowo z nazwy użytkownika dużą literą?';
+$l['style_your_nick_desc_shadowx'] = '<strong>X cienia</strong><br />
+Podaj prawidłową pozycje. Jest ona wymagana dla działania cienia. Wspierane formaty: 3px, 1.7cm, 14mm, 0.2in';
+$l['style_your_nick_desc_shadowy'] = '<strong>Y cienia</strong><br />
+Podaj prawidłową pozycje. Jest ona wymagana dla działania cienia. Wspierane formaty: 3px, 1.7cm, 14mm, 0.2in';
+$l['style_your_nick_desc_shadowlength'] = '<strong>Długość cienia/radius</strong><br />
+Podaj prawidłowy rozmiar. Wspierane formaty: 3px, 1.7cm, 14mm, 0.2in';
+$l['style_your_nick_desc_shadowcolor'] = '<strong>Kolor cienia</strong><br />
+Podaj prawidłowy kolor CSS. Wspierane formaty: ustalone nazwy (red, green, itd.), HEX #123DEF, RGB(255,255,255).';
+
+$l['style_your_nick_error_disallowed'] = 'Próbujesz zmienić opcję, która jest niedostępna dla Twojej grupy.';
+$l['style_your_nick_error_not_filled'] = 'Nie wypełniłeś żadnego pola. Przynajmniej jedno musi zostać zmienione.';
+$l['style_your_nick_error_trans'] = ' Przezroczyste kolory takie jak RGBA są niedozwolone.';
+$l['style_your_nick_error_max_px'] = ' Maksimum: {1}px.';
+$l['style_your_nick_error_min_px'] = ' Minimum: {1}px.';
+$l['style_your_nick_error_color'] = 'Nieprawidłowy kolor czcionki. Upewnij się, że wpisujesz poprawny format.{1}';
+$l['style_your_nick_error_background'] = 'Nieprawidłowy kolor tła. Upewnij się, że wpisujesz poprawny format.{1}';
+$l['style_your_nick_error_shadowcolor'] = 'Nieprawidłowy kolor cienia. Upewnij się, że wpisujesz poprawny format.{1}';
+$l['style_your_nick_error_shadowxy'] = 'Oba X i Y cienia muszą zostać podane.';
+$l['style_your_nick_error_size'] = 'Nieprawidłowy rozmiar czcionki. Upewnij się, że wpisujesz poprawny format.{1}{2}';
+$l['style_your_nick_error_shadowx'] = 'Nieprawidłowa pozycja X cienia. Upewnij się, że wpisujesz poprawny format.{1}{2}';
+$l['style_your_nick_error_shadowy'] = 'Nieprawidłowy pozycja Y cienia. Upewnij się, że wpisujesz poprawny format.{1}{2}';
+$l['style_your_nick_error_shadowlength'] = 'Nieprawidłowa długość cienia. Upewnij się, że wpisujesz poprawny format.{1}{2}';
+$l['style_your_nick_error_backgroundimgrepeat'] = 'Nie wybrałeś obrazka tła, który ma być powtarzany.';
+$l['style_your_nick_error_backgroundimg_wrong'] = 'Link do obrazka tła, który wskazałeś, nie prowadzi do prawidłowego obrazka.';
+$l['style_your_nick_error_backgroundimg_size'] = 'Obrazek tła jest za duży. Maksymalne dozwolone wymiary to {1}x{2}, a obrazek ma {3}x{4}.';
